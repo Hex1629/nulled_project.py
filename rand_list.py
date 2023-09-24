7c5ddb3adaaa236155b251a3c374b0529ad8dbd3
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2023-09-24 15:44:42 +0700

    Update rand_list.py

--- rand_list.py
+++ rand_list.py
@@ -1,5 +1,5 @@
 import random
 
 with open('got.Lst','a') as f:
-    for _ in range(250):
+    for _ in range(int(input('TOTAL NEED ?')):
      f.write(f'{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}/0\n')
